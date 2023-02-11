85a0d87516620976e6839b13f6874e43e7c74c55
HLimam <heithem.limame@gmail.com>
2023-02-11 20:52:40 +0100

    update config

--- src/utils/constants/url.ts
+++ src/utils/constants/url.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://backend-service/api/v1";
-export const WEBSOCKET_URL = "http://backend-service/websocket";
+export const API_BASE_URL = "http://${process.env.REACT_APP_PRODUCTION_DATABASE_URL}/api/v1";
+export const WEBSOCKET_URL = "http://${process.env.REACT_APP_PRODUCTION_DATABASE_URL}/websocket";
