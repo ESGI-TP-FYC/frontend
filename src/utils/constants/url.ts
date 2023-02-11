dd0f381311ed74cebc0dab5cfac4a9b35ca11b1d
HLimam <heithem.limame@gmail.com>
2023-02-11 17:49:36 +0100

    update config

--- src/utils/constants/url.ts
+++ src/utils/constants/url.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://esgi-fyc-app.link/backend/api/v1";
-export const WEBSOCKET_URL = "http://esgi-fyc-app.link/backend/websocket";
+export const API_BASE_URL = "http://aded28a8f346043b0b188053a94f9e6f-967579135.eu-west-1.elb.amazonaws.com/api/v1";
+export const WEBSOCKET_URL = "http://aded28a8f346043b0b188053a94f9e6f-967579135.eu-west-1.elb.amazonaws.com/websocket";
