5b7e3b51b52d77239c4a62952077193fb617efaa
HLimam <heithem.limame@gmail.com>
2023-02-11 15:01:28 +0100

    update config

--- src/utils/constants/url.ts
+++ src/utils/constants/url.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = "http://esgi-fyc-app.link/backend/api/v1";
-export const WEBSOCKET_URL = "http://esgi-fyc-app.link/socket/websocket";
+export const WEBSOCKET_URL = "http://esgi-fyc-app.link/backend/websocket";
