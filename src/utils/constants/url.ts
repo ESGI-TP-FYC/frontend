f50f3d66a186b3fbfa492dfbbdda62e410c88056
HLimam <heithem.limame@gmail.com>
2023-02-11 14:19:46 +0100

    update

--- src/utils/constants/url.ts
+++ src/utils/constants/url.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:8080/api/v1";
-export const WEBSOCKET_URL = "http://localhost:8080/websocket";
+export const API_BASE_URL = "http://ad86b255c1e5449078d26a8d93dbc4f7-352387646.eu-west-1.elb.amazonaws.com/api/v1";
+export const WEBSOCKET_URL = "http://ad86b255c1e5449078d26a8d93dbc4f7-352387646.eu-west-1.elb.amazonaws.com/websocket";
