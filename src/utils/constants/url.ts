bdec29395c89e37d10f9afbc9c6d64072f629a0b
HLimam <heithem.limame@gmail.com>
2023-02-12 15:52:26 +0100

    update config

--- src/utils/constants/url.ts
+++ src/utils/constants/url.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://"+process.env.REACT_APP_PRODUCTION_URL+"/api/v1";
-export const WEBSOCKET_URL = "http://"+process.env.REACT_APP_PRODUCTION_URL+"/websocket";
+export const API_BASE_URL = "http://a7bb58fe93fc44509b87e29ab8490680-342017876.eu-west-1.elb.amazonaws.com/api/v1";
+export const WEBSOCKET_URL = "http://a7bb58fe93fc44509b87e29ab8490680-342017876.eu-west-1.elb.amazonaws.com/websocket";
